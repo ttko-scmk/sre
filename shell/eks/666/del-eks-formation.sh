f09bc6ee486b6d85d38502e906b06ca36fd66dd8
DongNingYu <33471919+DongNingYu@users.noreply.github.com>
2026-08-20 16:22:39 +0800

    Update del-eks-formation.sh

--- shell/eks/666/del-eks-formation.sh
+++ shell/eks/666/del-eks-formation.sh
@@ -4,9 +4,9 @@ v=$1
 p=$2
 
 rm -rf /root/.kube/
-aws eks update-kubeconfig --region ap-east-1 --name scmk-666
+aws eks update-kubeconfig --region ap-east-1 --name dgp-live
 
 #exp
 #sh del-eks-formation.sh scmk 001
 
-aws cloudformation delete-stack --stack-name eksctl-scmk-666-nodegroup-"$v"-"$p"  --region ap-east-1
+aws cloudformation delete-stack --stack-name eksctl-dgp-live-nodegroup-"$v"-"$p"  --region ap-east-1
