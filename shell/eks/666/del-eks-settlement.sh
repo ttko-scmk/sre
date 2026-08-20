c42f07810da89f20ce7c6d6dd85713b92eaaed45
DongNingYu <33471919+DongNingYu@users.noreply.github.com>
2026-08-20 16:23:02 +0800

    Update del-eks-settlement.sh

--- shell/eks/666/del-eks-settlement.sh
+++ shell/eks/666/del-eks-settlement.sh
@@ -4,11 +4,11 @@ v=$1
 p=$2
 
 rm -rf /root/.kube/
-aws eks update-kubeconfig --region ap-east-1 --name scmk-666
+aws eks update-kubeconfig --region ap-east-1 --name dgp-live
 
 #exp
 #sh del-eks-settlement.sh cs 101
 #刪除POD 服務
 kubectl delete deployment.apps/"$v"-settlement-java
 #刪除NODE 服務
-aws cloudformation delete-stack --stack-name eksctl-scmk-666-nodegroup-"$v"-"$p"  --region ap-east-1
+aws cloudformation delete-stack --stack-name eksctl-dgp-live-nodegroup-"$v"-"$p"  --region ap-east-1
